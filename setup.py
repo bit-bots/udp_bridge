0e7e6ab72c5848670e4e721f41013dc8a149c547
Florian Vahl <git@flova.de>
2022-10-18 00:38:50 +0200

    Remove duplicated loc

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
           ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
           ('share/' + package_name, ['package.xml']),
-          ('share/' + package_name, ['package.xml']),
           ('share/' + package_name + '/config', glob.glob('config/*.yaml')),
           ('share/' + package_name + '/launch', glob.glob('launch/*.launch')),
       ],
