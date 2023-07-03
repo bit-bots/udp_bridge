4463cffe28cecd6789b7c478d2be7ed85aa22474
texhnolyze <joegriepen@gmail.com>
2023-07-03 13:24:28 +0200

    refactor: extract message (de)serialization and add typing
    while also trying to simplify some things

--- udp_bridge/aes_helper.py
+++ udp_bridge/aes_helper.py
@@ -1,4 +1,6 @@
 import base64
+from typing import Optional
+
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.hashes import SHA256
@@ -13,39 +15,33 @@ class AESCipher:
     It is safe to keep one object because the internal python cipher is not reused.
     """
 
-    def __init__(self, key: str):
+    def __init__(self, key: Optional[str]):
         """
         :param key: The passphrase used to encrypt and decrypt messages.
             If it is None, no encryption/decryption takes place
         """
-        if key is not None and len(key) == 0:
-            key = None
-
-        if key is None:
-            self.key = None
-        else:
+        if key is not None and len(key) > 0:
             # hash the key so that it has a fixed length and is urlsafe as required by the Fernet cipher
-            key = PBKDF2HMAC(
-                algorithm=SHA256(),
-                length=32,
-                salt=b"bit-bots",
-                backend=default_backend(),
-                iterations=10000
+            encryption_key = PBKDF2HMAC(
+                algorithm=SHA256(), length=32, salt=b"bit-bots", backend=default_backend(), iterations=10000
             ).derive(bytes(key, encoding="UTF-8"))
-            self.key = base64.urlsafe_b64encode(key)
+
+            self.encryption_key = base64.urlsafe_b64encode(encryption_key)
+        else:
+            self.encryption_key = None
 
     def encrypt(self, message: str) -> bytes:
         if message == "":
             raise ValueError("Cannot encrypt empty message")
-        if self.key is None:
+        if self.encryption_key is None:
             return bytes(message, encoding="UTF-8")
 
-        return Fernet(key=self.key).encrypt(bytes(message, encoding="UTF-8"))
+        return Fernet(key=self.encryption_key).encrypt(bytes(message, encoding="UTF-8"))
 
     def decrypt(self, enc: bytes) -> str:
         if len(enc) == 0:
             raise ValueError("Cannot decrypt empty data")
-        if self.key is None:
+        if self.encryption_key is None:
             return str(enc, encoding="UTF-8")
 
-        return str(Fernet(key=self.key).decrypt(enc), encoding="UTF-8")
+        return str(Fernet(key=self.encryption_key).decrypt(enc), encoding="UTF-8")
--- udp_bridge/sender.py
+++ udp_bridge/sender.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-import rclpy
-from rclpy.node import Node
-from rclpy.duration import Duration
-from ros2topic.api import get_topic_names_and_types, get_msg_class
+
+from queue import Empty, Full, Queue
 import socket
-import pickle
-import base64
-from threading import Lock, Thread
-from queue import Queue, Empty, Full
+from threading import Thread
+from typing import Optional
 
-from udp_bridge.aes_helper import AESCipher
-
+import rclpy
+from rclpy.duration import Duration
+from rclpy.node import Node
+from rclpy.subscription import Subscription
+from rclpy.timer import Timer
+from ros2topic.api import get_msg_class, get_topic_names
+from udp_bridge.message_handler import MessageHandler
 
 HOSTNAME = socket.gethostname()
-CIPHER = None
 
 
 class AutoSubscriber:
@@ -22,69 +22,68 @@ class AutoSubscriber:
     in a queue.
     """
 
-    def __init__(self, topic, queue_size, node:Node):
+    def __init__(self, topic: str, queue_size: int, message_handler: MessageHandler, node: Node):
         """
         :param topic: Topic to subscribe to
         :type topic: str
         :param queue_size: How many received messages should be buffered
         :type queue_size: int
         """
-        self.topic = topic
-        self.queue = Queue(queue_size)
-        self.node = node
-        self.timer = None
+        self.topic: str = topic
+        self.queue: Queue = Queue(queue_size)
+        self.message_handler: MessageHandler = message_handler
+        self.node: Node = node
+        self.timer: Optional[Timer] = None
 
-        self.__subscriber = None        # type: rospy.Subscriber
+        self.__subscriber: Optional[Subscription] = None
         self.__subscribe()
 
     def __subscribe(self, backoff=1.0):
         """
         Try to subscribe to the set topic
-        :param backoff: How long to wait until another try
+        :param backoff: How long to wait until another try (capped at 30s)
         """
+        if backoff > 30:
+            backoff = 30
+
         if self.timer:
             self.timer.cancel()
 
-        topics = get_topic_names_and_types(node=self.node)
         data_class = None
-        for topic, type_list in topics:
-            if topic == self.topic:
-                data_class = get_msg_class(self.node, topic)
-                self.node.get_logger().info(str(data_class))
-                break
+        topics = get_topic_names(node=self.node)
+        topic = next(filter(lambda t: t == self.topic, topics), None)
+
+        if topic is not None:
+            data_class = get_msg_class(self.node, topic)
+            self.node.get_logger().info(str(data_class))
 
         if data_class is not None:
             # topic is known
-            self.node.get_logger().info('Want to subscribe to topic {}'.format(self.topic))
+            self.node.get_logger().info(f"Want to subscribe to topic {self.topic}")
             self.__subscriber = self.node.create_subscription(data_class, self.topic, self.__message_callback, 1)
-            self.node.get_logger().info('Subscribed to topic {}'.format(self.topic))
-
+            self.node.get_logger().info(f"Subscribed to topic {self.topic}")
         else:
             # topic is not yet known
-            self.node.get_logger().info('Topic {} is not yet known. Retrying in {} seconds'.format(self.topic, int(backoff)))
-            if backoff > 30:
-                backoff = 30
-            self.timer = self.node.create_timer(
-                backoff,
-                lambda: self.__subscribe(backoff * 1.2)
-            )
+            self.node.get_logger().info(f"Topic {self.topic} is not yet known. Retrying in {backoff} seconds")
+            self.timer = self.node.create_timer(backoff, lambda: self.__subscribe(backoff * 1.2))
 
     def __message_callback(self, data):
-        serialized_data = base64.b64encode(pickle.dumps({
-            "data": data,
-            "topic": self.topic,
-            "hostname": HOSTNAME,
-        }, pickle.HIGHEST_PROTOCOL)).decode("ASCII")
-        enc_data = CIPHER.encrypt(serialized_data)
+        encrypted_msg = self.message_handler.encrypt_and_encode(
+            {
+                "data": data,
+                "topic": self.topic,
+                "hostname": HOSTNAME,
+            }
+        )
 
         try:
-            self.queue.put(enc_data, block=True, timeout=0.5)
+            self.queue.put(encrypted_msg, block=True, timeout=0.5)
         except Full:
-            self.node.get_logger().warn('Could enqueue new message of topic {}. Queue full.'.format(self.topic))
+            self.node.get_logger().warn(f"Could enqueue new message of topic {self.topic}. Queue full.")
 
 
-def validate_params(node:Node):
-    """:rtype: bool"""
+# @TODO: replace by usage of https://github.com/PickNikRobotics/generate_parameter_library
+def validate_params(node: Node) -> bool:
     result = True
     if not node.has_parameter("target_ips"):
         node.get_logger().fatal("parameter 'target_ips' not found")
@@ -93,12 +92,13 @@ def validate_params(node:Node):
     if not isinstance(target_ips, list):
         node.get_logger().fatal("parameter target_ips is not a list")
         result = False
-    for addr in target_ips:
-        try:
-            socket.inet_aton(addr)
-        except Exception as e:
-            node.get_logger().fatal("Cannot parse " + str(addr) + " as IP Address")
-            result = False
+    else:
+        for addr in target_ips:
+            try:
+                socket.inet_aton(addr)
+            except Exception as e:
+                node.get_logger().fatal("Cannot parse " + str(addr) + " as IP Address")
+                result = False
 
     if not node.has_parameter("port"):
         node.get_logger().fatal("parameter 'port' not found")
@@ -116,61 +116,78 @@ def validate_params(node:Node):
     if len(node.get_parameter("topics").value) == 0:
         node.get_logger().warn("parameter 'topics' is an empty list")
 
-    if not node.has_parameter('sender_queue_max_size'):
+    if not node.has_parameter("sender_queue_max_size"):
         node.get_logger().fatal("parameter 'sender_queue_max_size' not found")
         result = False
-    if not isinstance(node.get_parameter('sender_queue_max_size').value, int):
+    if not isinstance(node.get_parameter("sender_queue_max_size").value, int):
         node.get_logger().fatal("parameter 'sender_queue_max_size' is not an Integer")
         result = False
 
-    if not node.has_parameter('send_frequency'):
+    if not node.has_parameter("send_frequency"):
         node.get_logger().fatal("parameter 'send_frequency' not found")
         result = False
-    if not isinstance(node.get_parameter('send_frequency').value, float) \
-        and not isinstance(node.get_parameter('send_frequency').value, int):
+    if not isinstance(node.get_parameter("send_frequency").value, float) and not isinstance(
+        node.get_parameter("send_frequency").value, int
+    ):
         node.get_logger().fatal("parameter 'send_frequency' is not an Integer or Float")
         result = False
 
     return result
 
 
-def main():
-    global CIPHER
-    rclpy.init()
-    node = Node('udp_bridge_sender', automatically_declare_parameters_from_overrides=True)
-    thread = Thread(target=rclpy.spin, args=(node,))
+def setup_message_handler(node: Node) -> MessageHandler:
+    encryption_key: Optional[str] = None
+    if node.has_parameter("encryption_key"):
+        encryption_key = node.get_parameter("encryption_key").value
+
+    return MessageHandler(encryption_key)
+
+
+def run_spin_in_thread(node):
+    # Necessary in ROS2, else we get stuck
+    thread = Thread(target=rclpy.spin, args=[node], daemon=True)
     thread.start()
+
+
+def setup_udp_broadcast_socket() -> socket.socket:
+    sock = socket.socket(type=socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    return sock
+
+
+def main():
+    rclpy.init()
+    node = Node("udp_bridge_sender", automatically_declare_parameters_from_overrides=True)
+    run_spin_in_thread(node)
+
     if validate_params(node):
-        encryption_key = None
-        if node.has_parameter("encryption_key"):
-            encryption_key = node.get_parameter("encryption_key").value
-        CIPHER = AESCipher(encryption_key)
-        port = node.get_parameter("port").value
-        freq = node.get_parameter("send_frequency").value
-        targets = node.get_parameter('target_ips').value
-        max_queue_size = node.get_parameter('sender_queue_max_size').value
-        topics = node.get_parameter("topics").value
+        port: int = node.get_parameter("port").value
+        freq: float = node.get_parameter("send_frequency").value
+        targets: list[str] = node.get_parameter("target_ips").value
+        max_queue_size: int = node.get_parameter("sender_queue_max_size").value
+        topics: list[str] = node.get_parameter("topics").value
 
-        sock = socket.socket(type=socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        subscribers = []
+        message_handler = setup_message_handler(node)
+        sock = setup_udp_broadcast_socket()
 
-        for topic in topics:
-            subscribers.append(AutoSubscriber(topic, max_queue_size, node))
+        subscribers: list[AutoSubscriber] = list(
+            map(lambda topic: AutoSubscriber(topic, max_queue_size, message_handler, node), topics)
+        )
 
-        while rclpy.ok:
+        while rclpy.ok():
             for subscriber in subscribers:
                 try:
                     data = subscriber.queue.get_nowait()
 
                     for target in targets:
                         try:
-                            sock.sendto(data + b'\xff\xff\xff', (target, port))
+                            sock.sendto(data + MessageHandler.PACKAGE_DELIMITER, (target, port))
                         except Exception as e:
-                            node.get_logger().error('Could not send data of topic {} to {} with error {}'
-                                            .format(subscriber.topic, target, str(e)))
+                            node.get_logger().error(
+                                f"Could not send data of topic {subscriber.topic} to {target} with error {str(e)}"
+                            )
 
                 except Empty:
                     pass
 
-            node.get_clock().sleep_for(Duration(seconds=0, nanoseconds=int(1000000000 / freq)))
+            node.get_clock().sleep_for(Duration(nanoseconds=int(1000000000 / freq)))
